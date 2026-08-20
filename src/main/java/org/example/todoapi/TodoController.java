0c1d5d5c199fdbbd96b991c911a8579cce72d38a
Prasad5683 <piseprasad5683@gmail.com>
2026-08-21 00:17:40 +0530

    first commit

---
+++ src/main/java/org/example/todoapi/TodoController.java
@@ -0,0 +1,15 @@
+package org.example.todoapi;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class TodoController {
+    private static List<Todo> todos ;
+    public TodoController(){
+        todos= new ArrayList<>();
+        todos.add(new Todo(1,false,"Todo1",1));
+        todos.add(new Todo(2,false,"Todo2",2));
+    }
+}
